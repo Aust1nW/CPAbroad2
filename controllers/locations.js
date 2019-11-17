a4ea52b2eb206cccf6c9d22f0a670acf258c681a
Austin Woo <wooaustin0@gmail.com>
2019-11-17 15:28:54 -0800

    locations done

--- controllers/locations.js
+++ controllers/locations.js
@@ -2,72 +2,90 @@ const { locations } = require('../models');
 const Sequelize = require('sequelize');
 
 exports.getAllLocations = async(req, res) => {
-    try{
-        locations.findAll().then((allLocations) =>{
-            if(allLocations == null){
+    try {
+        locations.findAll().then((allLocations) => {
+            if (allLocations == null) {
                 res.json(null);
-            }
-            else{
+            } else {
                 res.json(allLocations);
             }
         });
-    }
-    catch(err) {
-            console.log(err);
-            res.send(404);
-    }
-}
-
-exports.getLocationByName = async(req,res) => {
-    try{
-        locations.findOne({where: {name: req.params.name}}).then((location) =>{
-            if(location == null){
-                res.json(null);
-            }
-            else{
-                res.json(location);
-            }
-        });
-    }
-    catch(err){
+    } catch (err) {
         console.log(err);
         res.send(404);
-        
+    }
+}
+
+exports.getLocationByName = async(req, res) => {
+    try {
+        locations.findOne({ where: { name: req.params.name } }).then((location) => {
+            if (location == null) {
+                res.json(null);
+            } else {
+                res.json(location);
+            }
+        });
+    } catch (err) {
+        console.log(err);
+        res.send(404);
+
 
     }
 }
 
-exports.getLocationByCountry = async(req,res) => {
-    try{
-        locations.findAll({where: {country: req.params.country}}).then((location)=>{
-            if(location == null){
+exports.getLocationByCountry = async(req, res) => {
+    try {
+        locations.findAll({ where: { country: req.params.country } }).then((location) => {
+            if (location == null) {
                 res.json(null);
-            }
-            else{
+            } else {
                 res.json(location);
             }
         });
-    }
-    catch(err){
+    } catch (err) {
         res.send(404);
         console.log(err);
     }
 }
 
-exports.getLocationByContinent = async(req,res) =>{
-    try{
-        locations.findAll({where: {continent: req.params.continent}}).then((location)=>{
-            if(location == null){
+exports.getLocationByContinent = async(req, res) => {
+    try {
+        locations.findAll({ where: { continent: req.params.continent } }).then((location) => {
+            if (location == null) {
                 res.json(null);
-            }
-            else{
+            } else {
                 res.json(location);
             }
         });
-    }
-    catch(err){
+    } catch (err) {
         res.send(404);
         console.log(err);
+    }
+}
+
+exports.removeLocationById = async(req, res) => {
+    try {
+        locations.findOne({ where: { id: req.params.id } }).then((location) => {
+            if (location == null) {
+                res.json(null);
+            } else {
+                res.json(location);
+            }
+        })
+    } catch (err) {
+        res.send(404);
+        console.log(err);
+    }
+}
+
+exports.addLocation = async(req, res) => {
+    console.log("Creating account " + JSON.stringify(req.body));
+    try {
+        locations.create(req.body).then((response) => {
+            res.json(response)
+        }).catch(error => res.status(400).send(error))
+    } catch (err) {
+        console.log(err)
     }
 }
 
@@ -75,6 +93,6 @@ exports.getLocationByContinent = async(req,res) =>{
 //    try {
 //        locations.findById(req.params.id).then((account)) =>{
 //            
-  //      }
+//      }
 //    }
 //
--- routes/locations.js
+++ routes/locations.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 let locationController = require('../controllers/locations');
 
 const locationRoutes = express.Router();
 
 locationRoutes.get('/all', locationController.getAllLocations);
-locationRoutes.get('/country/:country',locationController.getLocationByCountry);
-
-locationRoutes.get('/name/:name',locationController.getLocationByName);
+locationRoutes.get('/country/:country', locationController.getLocationByCountry);
+//locationRoutes.delete('/remove/id/:id', locationController.removeById);
+locationRoutes.get('/name/:name', locationController.getLocationByName);
+locationRoutes.post('/add', locationController.addLocation);
 //locationRoutes.post()*/
 
 module.exports = locationRoutes;
